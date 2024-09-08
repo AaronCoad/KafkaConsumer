1c2070f1f73292cb43e23dc5935b7b5534ebe710
Aaron Coad <coadaaron@gmail.com>
2024-09-08 19:13:16 +1000

    Simplification

--- src/KafkaConsumer/Program.cs
+++ src/KafkaConsumer/Program.cs
@@ -4,10 +4,6 @@ using Confluent.SchemaRegistry.Serdes;
 using Microsoft.Extensions.Configuration;
 
 var globalConfig = new ConfigurationBuilder().AddJsonFile($"{Environment.CurrentDirectory}\\appsettings.json").Build();
-var kafkaConfig = globalConfig.GetSection("Kafka");
-var cosmosConfig = globalConfig.GetSection("Cosmos");
 
-Consumer consumer = new Consumer(kafkaConfig);
-Repository repository = new Repository(cosmosConfig);
-Application application = new Application(consumer, repository);
-await application.Run(kafkaConfig["Topic"]!);
+Application application = new Application(new Consumer(globalConfig.GetSection("Kafka")), new Repository(globalConfig.GetSection("Cosmos")));
+await application.Run(globalConfig.GetSection("Kafka")["Topic"]!);
